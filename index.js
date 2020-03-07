98219c0f58a8d00b9da8102b96d2c0c78f12f7d6
Tami Shute <tamaralynnrenee@gmail.com>
2020-03-06 22:21:34 -0500

    initial prompts and axios call

---
+++ index.js
@@ -0,0 +1,65 @@
+  var inquirer = require("inquirer");
+  var fs = require("fs");
+  var axios = require("axios");
+
+
+
+  const userResponse = await inquirer
+      .prompt([{
+              type: "input",
+              message: "What's your Github username?",
+              name: "username"
+          },
+          {
+              type: "input",
+              message: "What's your project title?",
+              name: "projectTitle"
+          },
+          {
+              type: "input",
+              message: "Please give description of your project.",
+              name: "projectDescription"
+          },
+          {
+              type: "input",
+              message: "What are the steps required to install your project? Give step-by-step instructions to get the application running.",
+              name: "installation"
+          },
+          {
+              type: "input",
+              message: "Provide instructions and examples for use.",
+              name: "usage"
+          },
+          {
+              type: "input",
+              message: "List your collaborators, if any, with links to their GitHub profiles.\nIf you used any third-party assets that require attribution, list the creators with links to their primary platforms.",
+              name: "credits"
+          },
+          {
+              type: "input",
+              message: "If you would like to add a licence for you respository, please input the one you would like here. Otherwise, proceed by clikcing enter",
+              name: "license"
+          },
+          {
+              type: "input",
+              message: "If you would like write tests for your application. Then provide examples on how to run them. Otherwise, proceed by clikcing enter",
+              name: "tests"
+          },
+
+
+      ])
+
+  let userName = userResponse.username;
+  console.log(`Username: ${userName}`)
+
+  const apiURL = `https://api.github.com/users/${userName}`
+
+  let result = axios.get(apiURL)
+  const gitData = result.data;
+
+  const name = gitData.name;
+  const url = gitData.url;
+  const img = gitData.avatar_url;
+  const email = gitData.email;
+  console.log(`Name: ${name}`);
+  console.log(`url: ${url}`);
